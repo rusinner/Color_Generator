515ec19394a17da68dc0920d2717ea90279ce750
Theologos Mavropoulos <rusinner88@gmail.com>
2022-11-15 20:07:14 +0200

    3:25:11

--- color-generator/src/App.js
+++ color-generator/src/App.js
@@ -4,7 +4,42 @@ import "./App.css";
 import Values from "values.js";
 
 function App() {
-  return <div className="App"></div>;
+  const [color, setColor] = useState("");
+  const [error, setError] = useState(false);
+  const [list, setList] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    try {
+      let colors = new Values(color).all(10);
+    } catch (error) {
+      setError(true);
+      console.log(error);
+    }
+  };
+  return (
+    <>
+      <section className="container">
+        <h3>color generator :</h3>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+            placeholder="#f15025"
+            className={`${error ? "error" : null}`}
+          />
+          <button className="btn" type="submit">
+            submit
+          </button>
+        </form>
+      </section>
+
+      <section className="colors">
+        <h4>list</h4>
+      </section>
+    </>
+  );
 }
 
 export default App;
