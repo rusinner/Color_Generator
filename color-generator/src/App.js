db9bba0a9ed2c2d9a527f7cfb4ed974dab9be547
Theologos Mavropoulos <rusinner88@gmail.com>
2022-11-15 19:12:53 +0200

    setup

---
+++ color-generator/src/App.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import "./App.css";
+
+import Values from "values.js";
+
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
